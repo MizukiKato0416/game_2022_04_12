7af62c8e3f634a29afccb7bab3abcccf0500ab84
HatoriTaichi <201113@st.yoshida-g.ac.jp>
2022-04-15 14:41:47 +0900

    l

--- source code/road.cpp
+++ source code/road.cpp
@@ -36,7 +36,6 @@ HRESULT CRoad::Init(void)
 {
 	m_floor = CFloor::Create(m_pos, m_size, D3DXVECTOR3(0.0f, 0.0f, 0.0f));
 	m_floor->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("TEX_TYPE_DUNGEON_WALL"));
-	int n = 19;
 
 	switch (m_happening_type)
 	{
