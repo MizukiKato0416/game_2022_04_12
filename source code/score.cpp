a183f1e1b41d961dd6a0ce20cf173e0d90a4b823
mimimimimi0416 <201014@st.yoshida-g.ac.jp>
2022-05-17 10:01:12 +0900

    Update

--- source code/score.cpp
+++ source code/score.cpp
@@ -17,7 +17,9 @@
 //================================================
 #define SCORE_CONMA_NUM		(6)		//コンマをはさむ位置
 #define SCORE_CONMA_SIZE	(15.0f)	//コンマのサイズ
+#define SCORE_UNIT_SIZE		(60.0f)	//単位のサイズ
 #define SCORE_CONMA_POS_X	(5.0f)	//コンマの位置調整値
+#define SCORE_POS_SHIFT_X	(20.0f)	//スコアの位置をずらす量
 
 //================================================
 //デフォルトコンストラクタ
@@ -50,22 +52,28 @@ HRESULT CScore::Init()
 		//小数点をつけるため、SCORE_COMMA_NUMつ目の数字からずらす
 		if (nCntNumber >= SCORE_CONMA_NUM)
 		{
-			m_apNumber[nCntNumber] = CNumber::Create(D3DXVECTOR3(m_pos.x + 20.0f - (m_size.x * MAX_SCORE_POLYGON / 2) + (m_size.x * nCntNumber) + (m_size.x / 2.0f), m_pos.y, 0.0f),
-												                 m_size);
+			m_apNumber[nCntNumber] = CNumber::Create(D3DXVECTOR3(m_pos.x + SCORE_POS_SHIFT_X - (m_size.x * MAX_SCORE_POLYGON / 2) + (m_size.x * nCntNumber) + (m_size.x / 2.0f), m_pos.y, 0.0f),
+												     m_size);
 		}
 		else
 		{
 			m_apNumber[nCntNumber] = CNumber::Create(D3DXVECTOR3(m_pos.x - (m_size.x * MAX_SCORE_POLYGON / 2) + (m_size.x * nCntNumber) + (m_size.x / 2.0f), m_pos.y, 0.0f),
-												                 m_size);
+												     m_size);
 		}
 		m_apNumber[nCntNumber]->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("number.png"));
 	}
 
 	//コンマの生成
-	CObject2D *pObject2D = CObject2D::Create(D3DXVECTOR3(m_pos.x  - SCORE_CONMA_POS_X - (m_size.x * MAX_SCORE_POLYGON / 2) + (m_size.x * SCORE_CONMA_NUM) + (m_size.x / 2.0f), m_pos.y + m_size.y / 4.0f, 0.0f),
-												         D3DXVECTOR3(SCORE_CONMA_SIZE, SCORE_CONMA_SIZE, 0.0f),
-		                                                 static_cast<int>(CObject::PRIORITY::UI));
-	pObject2D->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("conma.png"));
+	CObject2D *pConma = CObject2D::Create(D3DXVECTOR3(m_pos.x  - SCORE_CONMA_POS_X - (m_size.x * MAX_SCORE_POLYGON / 2) + (m_size.x * SCORE_CONMA_NUM) + (m_size.x / 2.0f), m_pos.y + m_size.y / 4.0f, 0.0f),
+												      D3DXVECTOR3(SCORE_CONMA_SIZE, SCORE_CONMA_SIZE, 0.0f),
+		                                              static_cast<int>(CObject::PRIORITY::UI));
+	pConma->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("conma.png"));
+
+	//単位の生成
+	CObject2D *pUnit = CObject2D::Create(D3DXVECTOR3(m_pos.x + SCORE_POS_SHIFT_X - (m_size.x * MAX_SCORE_POLYGON / 2) + (m_size.x * MAX_SCORE_POLYGON) + (m_size.x / 2.0f), m_pos.y, 0.0f),
+												     D3DXVECTOR3(SCORE_UNIT_SIZE, SCORE_UNIT_SIZE, 0.0f),
+		                                             static_cast<int>(CObject::PRIORITY::UI));
+	pUnit->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("m.png"));
 
 	return S_OK;
 }
