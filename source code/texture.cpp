eae376fdb27a2c284493d73e26eac129b77b94ea
HatoriTaichi <201113@st.yoshida-g.ac.jp>
2022-05-30 14:35:35 +0900

    f

--- source code/texture.cpp
+++ source code/texture.cpp
@@ -50,33 +50,18 @@ CTexture::~CTexture()
 void CTexture::Init(void)
 {
 	LPDIRECT3DDEVICE9 pDevice;	// デバイスのポインタ
-	int count_tex = 0;			// テクスチャカウント様
-	int pas_size = sizeof(PAS_NAME) - 1;	// パスの文字数サイズ
+	int element_max;		// テクスチャカウント様
 	pDevice = CManager::GetInstance()->GetRenderer()->GetDevice();	// デバイスを取得する
 
-	pair<vector<string>, vector<string>> name_pas = CFileLoad::Load("data\\TEXTURE\\");
+	// ファイルを読み込む
+	m_file_name_pas = CFileLoad::Load("data\\TEXTURE\\");
+	element_max = m_file_name_pas.second.size();
 
-	// パスのファイル名を読み込む
-	for (const auto &file : recursive_directory_iterator("data/TEXTURE/"))
+	for (int count_element = 0; count_element < element_max; count_element++)
 	{
-		// パスの保存
-		m_file_name_pas.first.push_back(file.path().string());
-		m_file_name_pas.second.push_back(file.path().string());
-
-		// パスが混ざってたら
-		if (m_file_name_pas.second[count_tex].find(PAS_NAME) != string::npos)
-		{
-			for (int count_erase = 0; count_erase < pas_size; count_erase++)
-			{
-				// 要らないところを削除
-				m_file_name_pas.second[count_tex].erase(m_file_name_pas.second[count_tex].begin());
-			}
-		}
 		// 疑似列挙型を作る
-		m_tex_type[m_file_name_pas.second[count_tex]] = count_tex;
-		count_tex++;
+		m_tex_type[m_file_name_pas.second[count_element]] = count_element;
 	}
-
 	// パスとサイズを保存
 	m_pas = m_file_name_pas.first;
 	m_num_tex = m_file_name_pas.first.size();
